d18a3bbac42e1163274a1950192e96f3cd8508fb
Tapan Prakash T <tapanprakasht@gmail.com>
2015-05-02 18:49:27 +0530

    Added folder command

--- vcsinstaller.sh
+++ vcsinstaller.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+mkdir VisualStudioCode
+cd VisualStudioCode
 wget http://download.microsoft.com/download/0/D/5/0D57186C-834B-463A-AECB-BC55A8E466AE/VSCode-linux-x64.zip
 unzip VSCode-linux-x64.zip
+rm VSCode-linux-x64.zip
